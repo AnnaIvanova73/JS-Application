7fa53f5eb9859ee9da27104f19f37d0ea6647737
AnnaIvanova73 <allergictohumans73@gmail.com>
2021-07-01 00:21:41 +0300

    add finish task in lab - asynchronous programming

--- 2-Asynchronous Programming/lab/01.XHR/scripts/app.js
+++ 2-Asynchronous Programming/lab/01.XHR/scripts/app.js
@@ -1,3 +1,14 @@
 function loadRepos() {
-   console.log("TODO...");
+   const request = new XMLHttpRequest();
+
+   request.addEventListener('readystatechange', () => {
+      if(request.readyState === 4 && request.status === 200) {
+         let res = document.getElementById('res');
+         res.innerHTML = request.responseText
+      }
+   })
+   request.open('GET',`https://api.github.com/users/testnakov/repos`);
+   request.send();
+
 }
+
