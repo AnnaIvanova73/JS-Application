44b31662a265480870bcd7c402200eb137307936
AnnaIvanova73 <allergictohumans73@gmail.com>
2021-07-01 04:20:54 +0300

    add in 2-Async Programming/ex --> 01.Bus-Stop, 02.Bus-Schedule

--- 2-Asynchronous Programming/ex/02.Bus-Schedule/app.js
+++ 2-Asynchronous Programming/ex/02.Bus-Schedule/app.js
@@ -1,12 +1,51 @@
 function solve() {
+    const getElementsDom = () => {
+        return {
+            box: document.querySelector('#info > span.info'),
+            arriveBtn: document.querySelector('#arrive'),
+            departBtn: document.querySelector('#depart'),
+        }
+    };
+    const requestData = {id: "depot"};
+
+    const takeRepercussions = () => {
+        getElementsDom().box.textContent = 'Error';
+        getElementsDom().departBtn.disabled = true;
+        getElementsDom().arriveBtn.disabled = true;
+    }
 
     function depart() {
-        console.log('Depart TODO...');
+        engineStartPromiseChain().catch(takeRepercussions);
+        getElementsDom().departBtn.disabled = true;
+        getElementsDom().arriveBtn.disabled = false;
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        engineStartPromiseChain().catch(takeRepercussions);
     }
+
+    const renderDataOnDom = (data) => {
+        requestData.id = data.next;
+        getElementsDom().box.textContent = `Next stop ${data.name}`;
+    };
+
+    const requestFromApi = async (url) => {
+        let response = await fetch(url);
+        if (response.status !== 200) {
+            throw new Error('Error');
+        }
+        let jsonResponse = await response.json();
+        renderDataOnDom(jsonResponse);
+    };
+
+    const buildRequestUrl = (requestData) => {
+        return `http://localhost:3030/jsonstore/bus/schedule/${requestData.id}`;
+    };
+
+    const engineStartPromiseChain = async () => {
+        const requestUrl = buildRequestUrl(requestData);
+        await requestFromApi(requestUrl);
+    };
 
     return {
         depart,
