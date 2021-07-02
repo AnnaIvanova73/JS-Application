31af239acb3f047bcc8c3d555dfadf4cda9ec26a
AnnaIvanova73 <allergictohumans73@gmail.com>
2021-07-02 06:36:40 +0300

    add in 2-Async Programming/ex --> 03.Locked Profile resolve

--- 2-Asynchronous Programming/ex/04.Locked-Profile/app.js
+++ 2-Asynchronous Programming/ex/04.Locked-Profile/app.js
@@ -1,3 +1,53 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    const createDivProfileInfo = (blueprint, {age, email, username}, count) => {
+        let newDiv = blueprint.cloneNode(true);
+        newDiv.querySelector('input[type="text"]').value = username;
+        newDiv.querySelector('input[name="user1Email"]').value = email;
+        newDiv.querySelector('input[name="user1Age"]').value = age;
+        return newDiv;
+    }
+
+
+    const renderDataOnDom = (data) => {
+        let div = document.querySelector('div.profile');
+        let mainElement = document.querySelector('#main');
+        let arrDataUserInfo = Array.from(Object.entries(data));
+        let counterId = 2;
+        arrDataUserInfo.forEach((e) => {
+            let divProfile = createDivProfileInfo(div, e[1], counterId);
+            mainElement.appendChild(divProfile);
+            counterId++;
+        })
+        div.remove();
+    }
+    const requestDataFromApi = async (url) => {
+        let response = await fetch(url);
+        let responseJson = await response.json();
+        renderDataOnDom(responseJson);
+    }
+    const engine = async () => {
+        let requestUrl = `http://localhost:3030/jsonstore/advanced/profiles`
+        await requestDataFromApi(requestUrl);
+    }
+
+    Array.from(document.querySelectorAll(`#main`)).forEach(e => {
+        e.addEventListener('click',(e) =>{
+           let currDiv =  e.target.parentNode;
+           let radio = currDiv.querySelectorAll('input[type ="radio"]')[1];
+           if(e.target.textContent === 'Show more'){
+                    if(radio.checked){
+                        currDiv.querySelector('#user1HiddenFields').style.display = 'block';
+                        currDiv.querySelector('button').textContent = 'Hide it';
+                    }
+           }else{
+               if(radio.checked){
+                   currDiv.querySelector('#user1HiddenFields').style.display = 'none';
+                   currDiv.querySelector('button').textContent = 'Show more';
+               }
+           }
+        })
+    })
+
+    engine().catch();
 }
