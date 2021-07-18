feba6ac3c89628f648a6a3d41565c1a15c5b5408
AnnaIvanova73 <allergictohumans73@gmail.com>
2021-07-18 08:47:18 +0300

    add-task-done

--- 6-Client-Side-Rendering/ex/03.Search-in-List/search.js
+++ 6-Client-Side-Rendering/ex/03.Search-in-List/search.js
@@ -1,3 +1,36 @@
+import towns from './towns.js';
+import {html, render} from './node_modules/lit-html/lit-html.js'
+
+const createElements = (elements) => html`
+    <ul>
+        ${elements.map(e => html`
+            <li>${e}</li>`)}
+    </ul>
+`
+
 function search() {
-   // TODO
+    render(createElements(towns), document.querySelector('#result'));
+
+    document.querySelector('#searchText').addEventListener('click', (e) => {
+        Array.from(document.querySelectorAll('ul li')).forEach(e => e.classList.remove('active'));
+        document.querySelector('#result').lastChild.remove();
+    });
+
+    document.querySelector('button').addEventListener('click', () => {
+        let matches = 0;
+        const currCity = document.querySelector('#searchText');
+        Array.from(document.querySelectorAll('ul li')).forEach(e => {
+            if (e.textContent.includes(currCity.value)) {
+                e.classList.add('active');
+                matches++;
+            }
+        });
+        let text = matches ?
+            document.querySelector('#result').appendChild(document.createTextNode(`${matches === 1 ? `1 match found`
+                    : `${matches} matches found`}`)) : '';
+        currCity.value = '';
+    });
+
 }
+
+search();
